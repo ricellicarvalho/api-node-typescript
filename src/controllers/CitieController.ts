16e0f9348dd3b76fcce220fe97a91a62a78cc22a
Ricelli Carvalho <ricelli.martinsdecarvalho@gmail.com>
2023-01-25 16:45:14 -0300

    feat: adding library for validation

--- src/controllers/CitieController.ts
+++ src/controllers/CitieController.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from 'express';
+import { StatusCodes } from 'http-status-codes';
 
 interface ICitie {
   name: string;
@@ -12,6 +13,10 @@ class CitieController {
 
   async create(req: Request<{}, {}, ICitie>, res: Response) {
     const data: ICitie = req.body;
+
+    if (req.body.name === undefined) {
+      return res.status(StatusCodes.BAD_REQUEST).send('A propiedade nome existe');
+    }
     
     return res.json(data.name);
   }
